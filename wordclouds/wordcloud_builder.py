17919f325ca51f3944c6f07fcdf28d500a69d56a
lona9 <pilar.vasquez.h@gmail.com>
2022-04-13 12:45:18 -0400

    add docstrings

--- wordclouds/wordcloud_builder.py
+++ wordclouds/wordcloud_builder.py
@@ -7,13 +7,27 @@ from PIL import Image
 import os
 
 def create_wordcloud(file_name):
+    """
+    Processes a file text, and then outputs a word cloud image file.
+
+    Input:
+    file_name: path of the text file to be proccessed.
+
+    Output:
+    image_name: creates a new image with a word cloud of the input text.
+    """
+
+    #define stop words and extra noise worse
     stop_words = get_stop_words('english')
     additional_words = ["just", "said", "told", "like", "t", "s", "get", "time", "year", "got", "one", "know"]
     stop_words.extend(additional_words)
+
+    #read original text file and split words
     file = open(file_name, encoding="utf8")
     line = file.read()
     words = line.split()
 
+    #filter text based on stop words
     if file_name.startswith('nta'):
         image_name = "nta.png"
         with open('filteredtext_nta.txt', "a") as filtered_file:
@@ -34,14 +48,14 @@ def create_wordcloud(file_name):
         with open('filteredtext_yta.txt', 'r') as txt_file:
             filteredtext = txt_file.read()
 
-    #mask = np.array(Image.open('dinosaur.png'))
-    wordcloudd = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black',
+    #create wordcloud
+    create_cloud = WordCloud(width = 3000, height = 2000, random_state=1, background_color='black',
                         colormap='viridis', collocations=False,
                              stopwords=stop_words).generate(filteredtext)
-    # Display image
+    #save image
     plt.figure(figsize=(40, 30))
     plt.axis("off")
-    wordcloudd.to_file(image_name)
+    create_cloud.to_file(image_name)
 
 def main():
     for file in os.listdir("."):
